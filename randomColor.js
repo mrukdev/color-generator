9dc144bf2be204c306ed3f41ae602b6ac0bf04f8
mrukdev <damian890mk@gmail.com>
2020-04-07 01:15:08 +0200

    optimization code

--- randomColor.js
+++ randomColor.js
@@ -1,16 +1,47 @@
-var generateColors = (howManyColors) =>{
-    let colorList = [];
-    let color = '';
-    for(let i=0; i<howManyColors;i++){
-        do{
-            let firstColor = Math.floor(Math.random() * 256);
-            let secondColor = Math.floor(Math.random() * 256);
-            let thirdColor = Math.floor(Math.random() * 256);
-            color = `rgba(${firstColor},${secondColor}, ${thirdColor},1)`;
-        } while(colorList.indexOf(color)>-1)
+class Color{
+    constructor(red, green, blue){
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+    }
+}
 
+let colorCheck = (colorList, color) =>{
+    if(colorList.length==0)
+    {
+        console.log('colorListLength==0');
+        return true;
+    }
+    if(!color){
+        console.log('color false');
+        return false;
+    }
+    if(colorList.includes(color)){
+        return false;
+    }
+    return true;
+}
+
+let colorToRgbString = (colorList) =>{
+    let colorRgbString = [];
+    for(let i=0;i<colorList.length;i++){
+        let color = `rgba(${colorList[i].red}, ${colorList[i].green}, ${colorList[i].blue})`;
+        colorRgbString.push(color);
+    }
+    return colorRgbString;
+}
+
+let generateColors = (howManyColors) => {
+    let colorList = [];
+    let color;
+    for(let i=0; i<howManyColors;i++){
+        let color;        
+        do{
+            color = new Color(Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256))
+        }
+        while(!colorCheck(colorList, color));
         colorList.push(color);
     }
-    colorList.sort();
-    return colorList;
+    return colorToRgbString(colorList);
 }
+
